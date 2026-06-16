b92b95274a5f60429ef1c07288c83defb6874267
José Vitor <jose.vitorw018@gmail.com>
2026-06-15 23:32:04 -0300

    Finish

---
+++ src/main/java/com/vtr/springragollamapgvector/SpringRagOllamaPgvectorApplication.java
@@ -0,0 +1,13 @@
+package com.vtr.springragollamapgvector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRagOllamaPgvectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRagOllamaPgvectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vtr/springragollamapgvector/SpringRagOllamaPgvectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtr.springragollamapgvector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRagOllamaPgvectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
